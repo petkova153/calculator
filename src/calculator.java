349c74726a2144ca988a7cc49321b956b1510f2f
petko <petkova153@gmail.com>
2023-03-06 21:32:35 +0200

    calculator class update

--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class calculator {
     //psvm to start a main function
     public static void main(String[] args) {
         System.out.println("calculator...");
+        System.out.println("Calculating the sum of two numbers");
+        System.out.println("Provide first number:");
+        int num1;
+        int num2;
+        int sum;
+        // TODO explain object relation
+        Scanner inputreader;
+        inputreader = new Scanner(System.in);
+        num1 = inputreader.nextInt();
+        System.out.println("you provided nr " + num1);
+        num2 = inputreader.nextInt();
+        System.out.println("you provided nr " + num2);
+        sum = num1 + num2;
+        System.out.println("Sum is: " + sum);
     }
 }
